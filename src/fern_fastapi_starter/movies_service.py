098b5debd9930eab8a43597414a85f69725721f3
Zach Kirsch <zachkirsch@gmail.com>
2022-12-20 15:15:16 -0800

    Move types to be below errors

--- src/fern_fastapi_starter/movies_service.py
+++ src/fern_fastapi_starter/movies_service.py
@@ -1,17 +1,6 @@
-from .generated.fern import (
-    AbstractMoviesService,
-    Movie,
-    MovieDoesNotExistError,
-    MovieId,
-)
+from .generated.fern import AbstractMoviesService, Movie
 
 
 class MoviesService(AbstractMoviesService):
     def get_movie(self, *, movie_id: str) -> Movie:
-        if movie_id == "titanic":
-            return Movie(
-                id=MovieId.from_str("titantic"),
-                title="Titanic",
-                rating=9.8,
-            )
-        raise MovieDoesNotExistError(MovieId.from_str(movie_id))
+        return 42
