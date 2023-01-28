159206b1166e6d9ff2da5e75cf8396e1e9bf23f2
Zach Kirsch <zachkirsch@gmail.com>
2023-01-28 03:38:04 -0800

    Format more files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,11 +6,11 @@ import "./index.css";
 const root = document.getElementById("root");
 
 if (root == null) {
-  throw new Error("No element with ID #root.");
+    throw new Error("No element with ID #root.");
 }
 
 ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
