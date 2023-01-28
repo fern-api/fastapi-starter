c9b041c48464ce760ca925d6e0e4d0f21c38bc0f
Zach Kirsch <zachkirsch@gmail.com>
2023-01-28 03:35:36 -0800

    Fix prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,29 +3,29 @@ import "./App.css";
 import { ImdbApi, ImdbApiClient } from "./api";
 
 const BACKEND_CLIENT = new ImdbApiClient({
-	environment: "http://localhost:8080",
+    environment: "http://localhost:8080",
 });
 
 function App() {
-	const [movie, setMovie] = useState<ImdbApi.Movie>();
+    const [movie, setMovie] = useState<ImdbApi.Movie>();
 
-	const onClick = useCallback(async () => {
-		try {
-			const movie = await BACKEND_CLIENT.imdb.getMovie("goodwill-hunting");
-			setMovie(movie);
-		} catch (e) {
-			console.error("Failed to get movie", e);
-		}
-	}, []);
+    const onClick = useCallback(async () => {
+        try {
+            const movie = await BACKEND_CLIENT.imdb.getMovie("goodwill-hunting");
+            setMovie(movie);
+        } catch (e) {
+            console.error("Failed to get movie", e);
+        }
+    }, []);
 
-	return (
-		<div className="App">
-			<div className="button-container">
-				<button onClick={onClick}>Load movie</button>
-			</div>
-			{movie != null && <pre>{JSON.stringify(movie, undefined, 4)}</pre>}
-		</div>
-	);
+    return (
+        <div className="App">
+            <div className="button-container">
+                <button onClick={onClick}>Load movie</button>
+            </div>
+            {movie != null && <pre>{JSON.stringify(movie, undefined, 4)}</pre>}
+        </div>
+    );
 }
 
 export default App;
