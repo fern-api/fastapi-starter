dfdbe5188bfdde9a88a481d077f1ed4f93600e52
Zach Kirsch <zachkirsch@gmail.com>
2023-03-31 11:28:05 -0700

    Upgrade-fern-0.6.5-rc1

--- backend/src/fern_fastapi_starter/api/generated/resources/imdb/types/movie.py
+++ backend/src/fern_fastapi_starter/api/generated/resources/imdb/types/movie.py
@@ -4,7 +4,6 @@ import datetime as dt
 import typing
 
 import pydantic
-import typing_extensions
 
 from ....core.datetime_utils import serialize_datetime
 from .movie_id import MovieId
@@ -14,11 +13,6 @@ class Movie(pydantic.BaseModel):
     id: MovieId
     title: str
     rating: float = pydantic.Field(description=("The rating scale is one to five stars\n"))
-
-    class Partial(typing_extensions.TypedDict):
-        id: typing_extensions.NotRequired[MovieId]
-        title: typing_extensions.NotRequired[str]
-        rating: typing_extensions.NotRequired[float]
 
     def json(self, **kwargs: typing.Any) -> str:
         kwargs_with_defaults: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
@@ -30,6 +24,5 @@ class Movie(pydantic.BaseModel):
 
     class Config:
         frozen = True
-        allow_population_by_field_name = True
         extra = pydantic.Extra.forbid
         json_encoders = {dt.datetime: serialize_datetime}
